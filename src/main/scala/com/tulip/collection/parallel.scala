64b562513ef89c0bb2b5468e90737f7e18d31136
tulip <edelweissasuare@163.com>
2019-12-22 20:21:55 +0800

    upd

--- src/main/scala/com/tulip/collection/parallel.scala
+++ src/main/scala/com/tulip/collection/parallel.scala
@@ -11,10 +11,17 @@ object  Parallel {
     // Results for two runs:
     // " - 1 -  - 2 -  - 3 - 4 - 5 -  - 6 -  - 7 -  - 8 -  - 9 -  - 10"
     // " - 1 -  - 2 -  - 3 -  - 4 - 5 -  - 6 -  - 7 -  - 8 - 9 - 10"
+    //" - 1 -  - 2 -  - 3 -  - 4 -  - 5 -  - 6 -  - 7 -  - 8 -  - 9 -  - 10"
+    //" - 1 -  - 2 -  - 3 -  - 4 -  - 5 -  - 6 -  - 7 -  - 8 -  - 9 -  - 10"
+    val listPar = List(1,2,3,4,5,6)
 
-    list = ((1 to 10) fold 0) ((s1, s2) => s1 + s2)
-    // Result: 55
-    println(list)
+
+    var i = 0
+    while( i < 20 ) {
+      i += 1
+      println(listPar.par.fold(10)(_ + _))
+    }
+    //TODO 与内核有关，我最高81，即6个线程在运行，6*10+6!
 
     list = ((1 to 10).par fold 0) ((s1, s2) => s1 + s2)
     // Result: 55
